c969cc9fbb8308d299d5cc19313666332544213b
Mihail Fomin <107790477+mihail-fomin@users.noreply.github.com>
2023-02-17 20:12:21 +0500

    Count letters in a string

---
+++ count-letters.js
@@ -0,0 +1,31 @@
+// Count a number of each letter in a given string. Ignore letter casing and punctuation:
+
+const str = "To be, or not to be."
+
+// Expected answer:
+
+// {
+//   b: 2,
+//   e: 2,
+//   n: 1,
+//   o: 4,
+//   r: 1,
+//   t: 3,
+// }
+
+/*===============================================================================*/
+
+
+function countAndSortLetters(text) {
+	const chars = text
+		.toLowerCase()
+		.replace(/[^a-zа-я0-9]/g, '')
+		.split('')
+	let arr = Object.entries(chars.reduce((acc, el) => {
+		acc[el] = (acc[el] || 0) + 1;
+		return acc;
+	}, {}));
+	return Object.fromEntries(arr.sort((a, b) => a > b ? 1 : -1))
+}
+
+console.log(countAndSortLetters(str))
