7e42c761d0f89c46b184c264b49124682449f646
Mihail Fomin <107790477+mihail-fomin@users.noreply.github.com>
2023-02-26 12:35:21 +0500

    A function that returns Greatest common divisor

---
+++ get-divisors.js
@@ -0,0 +1,33 @@
+// Write a function that returns Greatest common divisor
+
+// Returns memoized version of f.
+// Works only if all f args are differ from each other
+function memoize(f) {
+	const cache = new Map() // cache stores in closure
+
+	return function (...args) {
+		// Creates string version of args to use as a cache key
+		let key = args.length + args.join('+')
+		if (cache.has(key)) {
+			return cache.get(key)
+		} else {
+			let result = f.apply(this, args)
+			cache.set(key, result)
+			return result
+		}
+	}
+}
+
+// Frunction to search Greatest common divisor using Evklid algorithm
+function gcd(a, b) {
+	if (a < b) {
+		[a, b] = [b, a]
+	}
+	while (b !== 0) {
+		[a, b] = [b, a % b]
+	}
+	return a
+}
+
+const gcdmemo = memoize(gcd)
+console.log(gcdmemo(85, 187)); // 17
