d5f946e56bca71035f0b060231d7574581e8a0df
Mihail Fomin <107790477+mihail-fomin@users.noreply.github.com>
2023-03-12 20:15:57 +0500

    updated logics

--- logger.js
+++ logger.js
@@ -1,65 +1,60 @@
 // Implement a Logger class that accepts a log level value 
 // and exposes 4 logging methods.
 
+const logLevels = {
+	error: 0,
+	warn: 1,
+	info: 2,
+	debug: 3,
+}
+
 class Logger {
 	constructor(logLevel) {
-		this.logLevel = logLevel
+		this.logLevel = logLevels[logLevel]
 	}
 
-	getNumber() {
-		let level;
-		switch (this.logLevel) {
-			case 'error':
-				level = 0
-				break;
-			case 'warn':
-				level = 1
-				break;
-			case 'info':
-				level = 2
-				break;
-			case 'debug':
-				level = 3
-				break;
-		}
-		return level
-	}
+	// getNumber() {
+	// 	switch (this.logLevel) {
+	// 		case 'error': return 0
+	// 		case 'warn': return 1
+	// 		case 'info': return 2
+	// 		case 'debug': return 3
+	// 	}
+	// }
 
 	debug(message) {
-		this.getNumber() === 3 ?
-			console.log(`${message} is logged`)
-			: console.log(`nothing is logged`)
+		if (this.logLevel === 3) {
+			console.log(message)
+		}
 	}
 
 	info(message) {
-		this.getNumber() >= 2 ?
-			console.log(`${message} is logged`)
-			: console.log(`nothing is logged`)
+		if (this.logLevel >= 2) {
+			console.log(message)
+		}
 	}
 
 	warn(message) {
-		this.getNumber() >= 1 ?
-			console.log(`${message} is logged`)
-			: console.log(`nothing is logged`)
+		if (this.logLevel >= 1) {
+			console.log(message)
+		}
 	}
 
 	error(message) {
-		this.getNumber() >= 0 ?
-			console.log(`${message} is logged`)
-			: console.log(`nothing is logged`)
+		if (this.logLevel >= 0) {
+			console.log(message)
+		}
 	}
 }
 
-const logger1 = new Logger("warn")
-
-
+let logger1 = new Logger("warn")
 logger1.warn("Some warning message") // => "Some warning message" is logged
 logger1.error("Some error message")  // => "Some error message" is logged
 logger1.info("Some info message")    // => nothing is logged
 logger1.debug("Some debug message")  // => nothing is logged
-
-const logger2 = new Logger("debug")
+console.log("----")
+let logger2 = new Logger("debug")
 logger2.warn("Some warning message") // => "Some warning message" is logged
 logger2.error("Some error message")  // => "Some error message" is logged
 logger2.info("Some info message")    // => "Some info message" is logged
-logger2.debug("Some debug message")  // => "Some debug message" is logged 
+logger2.debug("Some debug message")  // => "Some debug message" is logged
