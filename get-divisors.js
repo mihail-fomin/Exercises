5719bda293d6c7ebbb58ecdca8dd89b281c75e8d
Mihail Fomin <107790477+mihail-fomin@users.noreply.github.com>
2023-05-27 21:55:11 +0500

    Update get-divisors.js

--- get-divisors.js
+++ get-divisors.js
@@ -31,3 +31,16 @@ function gcd(a, b) {
 
 const gcdmemo = memoize(gcd)
 console.log(gcdmemo(85, 187)); // 17
+
+// AI solution
+
+function gcd(a, b) {
+  if (b == 0) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
+}
+
+console.log(gcd(60, 48)); // 12
+console.log(gcd(84, 45)); // 3
